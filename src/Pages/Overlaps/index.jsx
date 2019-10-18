b706131e13955a781558f829315305062e4de701
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-18 16:37:02 -0500

    Add Overlapping tutorial files

---
+++ src/Pages/Overlaps/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Overlaps.module.css'
+
+const Overlaps = () => {
+  return (
+    <div className={styles.body}>
+      <h1>Overlaps With Benefits</h1>
+    </div>
+  )
+}
+
+export default Overlaps
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,6 @@
 import HomePage from './HomePage'
 import BattlesIceCream from './BattlesIceCream'
 import ColourMagazine from './ColourMagazine'
+import Overlaps from './Overlaps'
 
-export { HomePage, BattlesIceCream, ColourMagazine }
+export { HomePage, BattlesIceCream, ColourMagazine, Overlaps }
